8bd5407f12391feea2432a5c97e3b613ca9e1764
XinyiYS <michaelxysg@gmail.com>
2020-03-18 13:18:50 +0800

    include a new HAI class

--- reaver/agents/__init__.py
+++ reaver/agents/__init__.py
@@ -2,11 +2,14 @@ from .base import *
 from .random import RandomAgent
 from .a2c import AdvantageActorCriticAgent
 from .ppo import ProximalPolicyOptimizationAgent
+from .hai import HumanAIInteractionAgent
 
 A2C = AdvantageActorCriticAgent
 PPO = ProximalPolicyOptimizationAgent
+HAI = HumanAIInteractionAgent
 
 registry = {
     'a2c': A2C,
-    'ppo': PPO
+    'ppo': PPO,
+    'hai': HAI,
 }
