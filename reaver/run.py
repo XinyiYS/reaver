03f42668522bc95cf6915d77545a962c0d129528
XinyiYS <michaelxysg@gmail.com>
2020-03-25 14:00:55 +0800

    format codestyle and add in model_varaible_scope

--- reaver/run.py
+++ reaver/run.py
@@ -5,25 +5,37 @@ from absl import app, flags
 
 import reaver as rvr
 
-flags.DEFINE_string('env', None, 'Either Gym env id or PySC2 map name to run agent in.')
-flags.DEFINE_string('agent', 'a2c', 'Name of the agent. Must be one of (a2c, ppo).')
+flags.DEFINE_string(
+    'env', None, 'Either Gym env id or PySC2 map name to run agent in.')
+flags.DEFINE_string(
+    'agent', 'a2c', 'Name of the agent. Must be one of (a2c, ppo).')
 
 flags.DEFINE_bool('render', False, 'Whether to render first(!) env.')
-flags.DEFINE_string('gpu', '0', 'GPU(s) id(s) to use. If not set TensorFlow will use CPU.')
+flags.DEFINE_string(
+    'gpu', '0', 'GPU(s) id(s) to use. If not set TensorFlow will use CPU.')
 
-flags.DEFINE_integer('n_envs', 4, 'Number of environments to execute in parallel.')
-flags.DEFINE_integer('n_updates', 1000000, 'Number of train updates (1 update has batch_sz * traj_len samples).')
+flags.DEFINE_integer(
+    'n_envs', 4, 'Number of environments to execute in parallel.')
+flags.DEFINE_integer('n_updates', 1000000,
+                     'Number of train updates (1 update has batch_sz * traj_len samples).')
 
-flags.DEFINE_integer('ckpt_freq', 500, 'Number of train updates per one checkpoint save.')
-flags.DEFINE_integer('log_freq', 100, 'Number of train updates per one console log.')
-flags.DEFINE_integer('log_eps_avg', 100, 'Number of episodes to average for performance stats.')
-flags.DEFINE_integer('max_ep_len', None, 'Max number of steps an agent can take in an episode.')
+flags.DEFINE_integer(
+    'ckpt_freq', 500, 'Number of train updates per one checkpoint save.')
+flags.DEFINE_integer(
+    'log_freq', 100, 'Number of train updates per one console log.')
+flags.DEFINE_integer('log_eps_avg', 100,
+                     'Number of episodes to average for performance stats.')
+flags.DEFINE_integer('max_ep_len', None,
+                     'Max number of steps an agent can take in an episode.')
 
-flags.DEFINE_string('results_dir', 'results', 'Directory for model weights, train logs, etc.')
-flags.DEFINE_string('experiment', None, 'Name of the experiment. Datetime by default.')
+flags.DEFINE_string('results_dir', 'results',
+                    'Directory for model weights, train logs, etc.')
+flags.DEFINE_string('experiment', None,
+                    'Name of the experiment. Datetime by default.')
 
 flags.DEFINE_multi_string('gin_files', [], 'List of path(s) to gin config(s).')
-flags.DEFINE_multi_string('gin_bindings', [], 'Gin bindings to override config values.')
+flags.DEFINE_multi_string(
+    'gin_bindings', [], 'Gin bindings to override config values.')
 
 flags.DEFINE_bool('restore', False,
                   'Restore & continue previously executed experiment. '
@@ -60,16 +72,20 @@ def main(argv):
         args.log_freq = 1
         args.restore = True
 
-    expt = rvr.utils.Experiment(args.results_dir, args.env, args.agent, args.experiment, args.restore)
+    expt = rvr.utils.Experiment(
+        args.results_dir, args.env, args.agent, args.experiment, args.restore)
 
-    gin_files = rvr.utils.find_configs(args.env, os.path.dirname(os.path.abspath(__file__)))
+    gin_files = rvr.utils.find_configs(
+        args.env, os.path.dirname(os.path.abspath(__file__)))
     if args.restore:
         gin_files += [expt.config_path]
     gin_files += args.gin_files
 
     if not args.gpu:
-        args.gin_bindings.append("build_cnn_nature.data_format = 'channels_last'")
-        args.gin_bindings.append("build_fully_conv.data_format = 'channels_last'")
+        args.gin_bindings.append(
+            "build_cnn_nature.data_format = 'channels_last'")
+        args.gin_bindings.append(
+            "build_fully_conv.data_format = 'channels_last'")
 
     gin.parse_config_files_and_bindings(gin_files, args.gin_bindings)
     args.n_envs = min(args.n_envs, gin.query_parameter('ACAgent.batch_sz'))
@@ -79,20 +95,29 @@ def main(argv):
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
     # sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-    
-    sess_mgr = rvr.utils.tensorflow.SessionManager(sess, expt.path, args.ckpt_freq, training_enabled=not args.test)
+
+    model_variable_scope = "{}_{}".format(str(args.env), str(args.agent))
+    sess_mgr = rvr.utils.tensorflow.SessionManager(sess,
+                                                   expt.path,
+                                                   args.ckpt_freq,
+                                                   model_variable_scope=model_variable_scope,
+                                                   training_enabled=not args.test)
 
     env_cls = rvr.envs.GymEnv if '-v' in args.env else rvr.envs.SC2Env
     env = env_cls(args.env, args.render, max_ep_len=args.max_ep_len)
 
-    agent = rvr.agents.registry[args.agent](env.obs_spec(), env.act_spec(), sess_mgr=sess_mgr, n_envs=args.n_envs)
-    agent.logger = rvr.utils.StreamLogger(args.n_envs, args.log_freq, args.log_eps_avg, sess_mgr, expt.log_path)
+    # use args.env and args.agent as the model_variable_scope
+    agent = rvr.agents.registry[args.agent](
+        env.obs_spec(), env.act_spec(), sess_mgr=sess_mgr, n_envs=args.n_envs)
+    agent.logger = rvr.utils.StreamLogger(
+        args.n_envs, args.log_freq, args.log_eps_avg, sess_mgr, expt.log_path)
 
     if sess_mgr.training_enabled:
         expt.save_gin_config()
         expt.save_model_summary(agent.model)
 
-    agent.run(env, args.n_updates * agent.traj_len * agent.batch_sz // args.n_envs)
+    agent.run(env, args.n_updates * agent.traj_len *
+              agent.batch_sz // args.n_envs)
 
 
 if __name__ == '__main__':
